a119ea77599e669b1a96c89071e60cadd17085cc
Andrew Forward <aforward@gmail.com>
2020-03-22 21:50:39 -0400

    Check token against clients table

--- public/api.php
+++ public/api.php
@@ -6,15 +6,21 @@ header("Content-Type: application/json");
 
 if (isset($headers["Authentication"])) {
     list($type, $token) = explode(" ", $headers["Authentication"], 2);
-    if ($token != "professorcharlesxavier") {
-      http_response_code(401);
-      $reply = [
-          "error" => "Invalid token.",
-          "token" => $token,
-          "type" => $type,
-      ];
-      echo json_encode($reply);
-      exit;
+
+    $dbconn = pg_connect("host=localhost port=5432 dbname=phpapp");
+    $sql = 'SELECT name, data FROM clients WHERE token = $1';
+    $result = pg_query_params($dbconn, $sql, [$token]);
+    $data = pg_fetch_all($result);
+
+    if (empty($data)) {
+        http_response_code(401);
+        $reply = [
+            "error" => "Invalid token.",
+            "token" => $token,
+            "type" => $type,
+        ];
+        echo json_encode($reply);
+        exit;
     }
 } else {
     http_response_code(400);
