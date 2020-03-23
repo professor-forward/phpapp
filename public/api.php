59e42bfc29889e7bbbe844cb56af7de3aea8dc95
Andrew Forward <aforward@gmail.com>
2020-03-22 21:10:59 -0400

    Provide the X-Men names in the API

--- public/api.php
+++ public/api.php
@@ -1,5 +1,27 @@
 <?php
 
-$reply = getallheaders();
+$headers = getallheaders();
+
+if (isset($headers["X-Men"])) {
+    http_response_code(200);
+    switch($mutant = $headers["X-Men"]) {
+    case "Wolverine":
+        $name = "Logan";
+        break;
+    case "Magento":
+        $name = "Eric";
+        break;
+    default:
+        $name = "Unknown";
+    }
+    $reply = ["mutant" => $mutant, "name" => $name];
+} else {
+    http_response_code(400);
+    $reply = [
+        "error" => "Please provide an X-Men mutant and reveal their human name.",
+        "headers" => $headers,
+    ];
+}
+
 header("Content-Type: application/json");
 echo json_encode($reply);
