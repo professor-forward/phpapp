b564a9230c9cb1ef5d4ff3271af01ea0891f78eb
Andrew Forward <aforward@gmail.com>
2020-03-22 21:42:47 -0400

    Authenticate the API with professorcharlesxavier

--- public/api.php
+++ public/api.php
@@ -2,26 +2,50 @@
 
 $headers = getallheaders();
 
-if (isset($headers["X-Men"])) {
-    http_response_code(200);
-    switch($mutant = $headers["X-Men"]) {
-    case "Wolverine":
-        $name = "Logan";
-        break;
-    case "Magento":
-        $name = "Eric";
-        break;
-    default:
-        $name = "Unknown";
+header("Content-Type: application/json");
+
+if (isset($headers["Authentication"])) {
+    list($type, $token) = explode(" ", $headers["Authentication"], 2);
+    if ($token != "professorcharlesxavier") {
+      http_response_code(401);
+      $reply = [
+          "error" => "Invalid token.",
+          "token" => $token,
+          "type" => $type,
+      ];
+      echo json_encode($reply);
+      exit;
     }
-    $reply = ["mutant" => $mutant, "name" => $name];
 } else {
+    http_response_code(400);
+    $reply = [
+        "error" => "Please provide a valid Authentication header for this API.",
+        "headers" => $headers,
+    ];
+    echo json_encode($reply);
+    exit;
+}
+
+if (!isset($headers["X-Men"])) {
     http_response_code(400);
     $reply = [
         "error" => "Please provide an X-Men mutant and reveal their human name.",
         "headers" => $headers,
     ];
+    echo json_encode($reply);
+    exit;
 }
 
-header("Content-Type: application/json");
+http_response_code(200);
+switch($mutant = $headers["X-Men"]) {
+case "Wolverine":
+    $name = "Logan";
+    break;
+case "Magento":
+    $name = "Eric";
+    break;
+default:
+    $name = "Unknown";
+}
+$reply = ["mutant" => $mutant, "name" => $name];
 echo json_encode($reply);
